1446a201a2e951e44564eacd568f23ff3719531e
Tom Willmot <tom@humanmade.co.uk>
2011-11-17 11:25:43 +0000

    Add support for WP_DEBUG_DISPLAY & Add ability to ignore PHP Notices

--- debug-bar/panels/class-debug-bar-php.php
+++ debug-bar/panels/class-debug-bar-php.php
@@ -1,21 +1,33 @@
 <?php
 
 class Debug_Bar_PHP extends Debug_Bar_Panel {
+	
 	var $warnings = array();
 	var $notices = array();
+	var $ignored = array();
 	var $real_error_handler = array();
 
 	function init() {
+		
 		if ( ! WP_DEBUG )
 			return false;
 
 		$this->title( __('Notices / Warnings', 'debug-bar') );
-
+		
 		$this->real_error_handler = set_error_handler( array( &$this, 'error_handler' ) );
+		
+		$this->ignored = array();
+
+		if ( !empty( $_COOKIE['debug-bar-ignored-notices'] ) )
+			$this->ignored = json_decode( stripslashes( $_COOKIE['debug-bar-ignored-notices'] ), true );
+
+		
+		add_action( 'wp_ajax_ignore_notice', array( &$this, 'ajax' ) );
+		
 	}
 
 	function prerender() {
-		$this->set_visible( count( $this->notices ) || count( $this->warnings ) );
+		$this->set_visible( count( $this->notices ) || count( $this->warnings ) || count( $this->ignored ) );
 	}
 
 	function debug_bar_classes( $classes ) {
@@ -26,8 +38,13 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 		return $classes;
 	}
 
-	function error_handler( $type, $message, $file, $line ) {
+	function error_handler( $type, $message, $file, $line ) {	
+		
+		
 		$_key = md5( $file . ':' . $line . ':' . $message );
+		
+		if ( in_array( $_key, $this->ignored ) )
+			return ! WP_DEBUG_DISPLAY;
 
 		switch ( $type ) {
 			case E_WARNING :
@@ -50,16 +67,22 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 				break;
 		}
 
-		if ( null != $this->real_error_handler )
+		if ( null != $this->real_error_handler && WP_DEBUG_DISPLAY )
 			return call_user_func( $this->real_error_handler, $type, $message, $file, $line );
 		else
-			return false;
+			return ! WP_DEBUG_DISPLAY;
 	}
 
 	function render() {
+		
 		echo "<div id='debug-bar-php'>";
-		echo '<h2><span>Total Warnings:</span>' . number_format( count( $this->warnings ) ) . "</h2>\n";
-		echo '<h2><span>Total Notices:</span>' . number_format( count( $this->notices ) ) . "</h2>\n";
+		echo '<h2><span>Total Warnings:</span><strong>' . number_format( count( $this->warnings ) ) . "</strong></h2>\n";
+		echo '<h2><span>Total Notices:</span><strong>' . number_format( count( $this->notices ) ) . "</strong></h2>\n";
+		echo '<h2><span>Total Ignored:</span><strong>' . number_format( count( $this->ignored ) ) . "</strong></h2>\n";
+		
+		if ( $this->ignored )
+			echo '<a class="reset-ignored" href="">Reset Ignored Notices</a>';
+		
 		if ( count( $this->warnings ) ) {
 			echo '<ol class="debug-bar-php-list">';
 			foreach ( $this->warnings as $location_message) {
@@ -68,17 +91,20 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 			}
 			echo '</ol>';
 		}
+		
 		if ( count( $this->notices ) ) {
 			echo '<ol class="debug-bar-php-list">';
-			foreach ( $this->notices as $location_message) {
+			foreach ( $this->notices as $key => $location_message) {
 				list( $location, $message) = $location_message;
-				echo "<li  class='debug-bar-php-notice'>NOTICE: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+				echo "<li data-key='key_$key' class='debug-bar-php-notice'>NOTICE: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "<a href=\"#\">ignore</a></li>";
 			}
 			echo '</ol>';
 		}
+		
 		echo "</div>";
 
 	}
+
 }
 
 ?>
