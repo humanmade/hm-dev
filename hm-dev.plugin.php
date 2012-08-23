6b72bf5165c0c3b2b29d36483004c2a83abbdbaa
Tom Willmot <tom@humanmade.co.uk>
2012-08-23 23:20:49 +0100

    Prepend "Dev •" to the <title /> and admin bar site name menu so it's obvious your on dev with hm-dev active

--- hm-dev.plugin.php
+++ hm-dev.plugin.php
@@ -23,3 +23,26 @@ if ( file_exists( HM_DEV_PATH . 'timestack/time-stack.php' ) && ! class_exists( 
 
 // Load the import, export commands
 include_once( HM_DEV_PATH . 'hm-dev.wp-cli.import.php' );
+
+// Hook in and prefix the <title /> and admin_bar site name with DEV ?
+add_filter( 'wp_title', $dev_title = function( $title ) {
+
+	$mark = 'DEV &bull; ';
+
+	// Make sure it isn't added twice
+	return  $mark . str_ireplace( $mark, '', $title );
+
+} );
+
+add_filter( 'admin_title', $dev_title );
+
+add_filter( 'admin_bar_menu', function() use ( $dev_title ) {
+
+	global $wp_admin_bar;
+
+	$wp_admin_bar->add_node( array(
+		'id'	=> 'site-name',
+		'title'	=> $dev_title( $wp_admin_bar->get_node( 'site-name' )->title )
+	) );
+
+}, 31 );
