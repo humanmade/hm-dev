9006878f63caf200bd8becdc25a7484230c1726a
Tom Willmot <tom@humanmade.co.uk>
2012-08-24 01:30:06 +0100

    Move some files around and re-factor

--- hm-dev.plugin.php
+++ hm-dev.plugin.php
@@ -10,18 +10,19 @@ Version: 1.3
 define( 'HM_DEV_PATH', plugin_dir_path( __FILE__ ) );
 define( 'HM_DEV_URL', plugin_dir_url( __FILE__ ) );
 
-include_once( HM_DEV_PATH . 'hm-dev.debug.php' );
-include_once( HM_DEV_PATH . 'hm-dev.mail.php' );
+require_once( HM_DEV_PATH . 'hm-dev.debug.php' );
+require_once( HM_DEV_PATH . 'hm-dev.mail.php' );
 
 // Load the unit tests
-include_once( HM_DEV_PATH . 'hm-dev.wp-unit.php' );
+require_once( HM_DEV_PATH . 'wp-unit/wp-unit.plugin.php' );
+require_once( HM_DEV_PATH . 'hm-dev.wp-cli.test.php' );
 
 // Load the time stack
 if ( file_exists( HM_DEV_PATH . 'timestack/time-stack.php' ) && ! class_exists( 'HM_Time_Stack' ) )
-	include_once( HM_DEV_PATH . 'timestack/time-stack.php' );
+	require_once( HM_DEV_PATH . 'timestack/time-stack.php' );
 
 // Load the import, export commands
-include_once( HM_DEV_PATH . 'hm-dev.wp-cli.import.php' );
+require_once( HM_DEV_PATH . 'hm-dev.wp-cli.import.php' );
 
 // Hook in and prefix the <title /> and admin_bar site name with DEV ?
 add_filter( 'wp_title', $dev_title = function( $title ) {
--- hm-dev.wp-cli.test.php
+++ hm-dev.wp-cli.test.php
@@ -1,27 +1,16 @@
 <?php
 
-require_once( dirname( __FILE__ ) . '/wp-unit/WPUnit.php' );
-
-/*
- * Author: Joe Hoyle
- * URL: joehoyle.co.uk
- * Contact: joe@humanmade.co.uk
- */
-
-// Add the command to the wp-cli, only if the plugin is loaded
-if ( function_exists( 'wp_unit_add_pages' ) && class_exists( 'WP_CLI' ) ) {
+// Add the command to the wp-cli
+if ( class_exists( 'WP_CLI' ) )
 	WP_CLI::addCommand( 'test', 'WPUnitCommand' );
-}
+
+if ( class_exists( 'WP_CLI_Command' ) ) :
 
 /**
- * WP Unit
+ * WPUnitCommand class.
  *
- * @package wp-cli
- * @subpackage commands/community
- * @author Joe Hoyle
+ * @extends WP_CLI_Command
  */
-
-if( class_exists( 'WP_CLI_Command' ) ) :
 class WPUnitCommand extends WP_CLI_Command {
 
 	var $testCases;
@@ -35,18 +24,21 @@ class WPUnitCommand extends WP_CLI_Command {
 
 	public function show( $args = array() ) {
 
-		$files = wptest_get_all_test_files(DIR_TESTCASE);
+		$files = wptest_get_all_test_files( DIR_TESTCASE );
 
-		foreach ($files as $file) {
+		foreach ( $files as $file ) {
 
+			// Skip example tests
 			if ( in_array( basename( $file ), array( 'MyTest.php', 'MyTestSecond.php' ) ) )
 				continue;
 
-			require_once($file);
+			require_once( $file );
 
 		}
 
 		$tests = wptest_get_all_test_cases();
+
+		WP_CLI::line( '' );
 
 		foreach( $tests as $t )
 			WP_CLI::line( $t );
@@ -59,76 +51,75 @@ class WPUnitCommand extends WP_CLI_Command {
 
 		$test = isset( $args[0] ) ? $args[0] : null;
 
-		$files = wptest_get_all_test_files(DIR_TESTCASE);
+		$files = wptest_get_all_test_files( DIR_TESTCASE );
 
-		foreach ($files as $file) {
+		foreach ( $files as $file ) {
 
+			// Skip the example tests
 			if ( in_array( basename( $file ), array( 'MyTest.php', 'MyTestSecond.php' ) ) )
 				continue;
 
-			require_once($file);
+			require_once( $file );
 
 		}
 
-		if( $test == null ) {
+		if ( empty( $test ) ) {
 			$this->testCases = wptest_get_all_test_cases();
+
 		} else {
 
-			if( strpos( $test, '*' ) ) {
+			if ( strpos( $test, '*' ) ) {
 
-				foreach( wptest_get_all_test_cases() as $_test )
-					if( preg_match( '/' . str_replace( '*', '([.]*?)', $test ) . '/', $_test ) )
+				foreach ( wptest_get_all_test_cases() as $_test )
+					if ( preg_match( '/' . str_replace( '*', '([.]*?)', $test ) . '/', $_test ) )
 						$this->testCases[] = $_test;
 
 			} else {
 
 				$this->testCases = array( $test );
+
 			}
 		}
 
-		// run the tests and print the results
-
+		// Run the tests and print the results
 		$result = new PHPUnit_Framework_TestResult;
 
-		list ($result, $printer) = $this->_run_tests($this->testCases, @$opts['t']);
+		list ( $result, $printer ) = $this->_run_tests( $this->testCases );
 
 		$this->_output_results( $result );
 
 	}
 
-	private function _run_tests($classes, $classname='') {
+	private function _run_tests( $classes, $classname = '' ) {
+
 		$suite = new PHPUnit_Framework_TestSuite();
 
 		// Turn off BackUpGlobal until https://github.com/sebastianbergmann/phpunit/issues/451 is fixed
 		$suite->setBackupGlobals( false );
 
-		foreach ($classes as $testcase)
-		{
-			if (!$classname or strtolower($testcase) == strtolower($classname)) {
-				$suite->addTestSuite($testcase);
-			}
-		}
+		foreach ( $classes as $testcase )
+			if ( ! $classname or strtolower( $testcase ) === strtolower( $classname ) )
+				$suite->addTestSuite( $testcase );
 
-		#return PHPUnit::run($suite);
 		$result = new PHPUnit_Framework_TestResult;
 
-		require_once('PHPUnit/TextUI/ResultPrinter.php');
+		require_once( 'PHPUnit/TextUI/ResultPrinter.php' );
 
 		$this->printer = new WPUnitCommandResultsPrinter();
-		$result->addListener($this->printer);
+		$result->addListener( $this->printer );
 
-		return array($suite->run($result), $this->printer);
+		return array( $suite->run( $result ), $this->printer );
 	}
+
 
 	private function _output_results( $result ) {
 
 		WP_CLI::line( '' );
 
-			WP_CLI::line( sprintf( 'Ran %d tests. %s Failed, %d Skipped, %d Passed',
-				count( $this->printer->failed_tests ) + count( $this->printer->skipped_tests ) + count( $this->printer->passed_tests ),
-				count( $this->printer->failed_tests ),
-				count( $this->printer->skipped_tests ),
-				count( $this->printer->passed_tests ) ) );
+		WP_CLI::line( sprintf( 'Ran %d tests. %%R%d%%n Failed, %%Y%d%%n Skipped, %%G%d%%n Passed', count( $this->printer->failed_tests ) + count( $this->printer->skipped_tests ) + count( $this->printer->passed_tests ),count( $this->printer->failed_tests ), count( $this->printer->skipped_tests ), count( $this->printer->passed_tests ) ) );
+
+		WP_CLI::line( '' );
+
 	}
 
 }
@@ -140,31 +131,33 @@ class WPUnitCommandResultsPrinter extends PHPUnit_TextUI_ResultPrinter implement
 	var $passed_tests;
 	var $current_test_suite;
 
-    public function printResult(PHPUnit_Framework_TestResult $result) {}
+    public function printResult( PHPUnit_Framework_TestResult $result ) {}
 
-    public function startTestSuite(PHPUnit_Framework_TestSuite $suite) {
+    public function startTestSuite( PHPUnit_Framework_TestSuite $suite ) {
+
     	$name = preg_replace( '(^.*::(.*?)$)', '\\1', $suite->getName() );
 
     	$this->current_test_suite = $name;
-    	WP_CLI::line( '' );
+
     	WP_CLI::line( $name );
+
     }
 
-    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time) {
+    public function addFailure( PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time ) {
 
     	$name = strpos( $test->getName(), '::' ) ? $test->getName() : $this->current_test_suite . '::' . $test->getName();
     	$this->failed_tests[$name] = $e->toString();
 
     }
 
-	public function addError(PHPUnit_Framework_Test $test, Exception $e, $time) {
+	public function addError( PHPUnit_Framework_Test $test, Exception $e, $time ) {
 
         $name = strpos( $test->getName(), '::' ) ? $test->getName() : $this->current_test_suite . '::' . $test->getName();
         $this->failed_tests[$name] = 'Script Error: ' . $e->getMessage() . ' [file: ' . $e->getFile() . ' line: ' . $e->getLine() . ' backtrace: ' . $e->getTraceAsString() . ']';
 
     }
 
-	public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
+	public function addSkippedTest( PHPUnit_Framework_Test $test, Exception $e, $time ) {
 
 		$name = strpos( $test->getName(), '::' ) ? $test->getName() : $this->current_test_suite . '::' . $test->getName();
         $this->skipped_tests[$name] = 'Skipped message: ' . $e->getMessage();
@@ -172,46 +165,47 @@ class WPUnitCommandResultsPrinter extends PHPUnit_TextUI_ResultPrinter implement
 
 	}
 
-	public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
+	public function addIncompleteTest( PHPUnit_Framework_Test $test, Exception $e, $time ) {
 
 		$name = strpos( $test->getName(), '::' ) ? $test->getName() : $this->current_test_suite . '::' . $test->getName();
         $this->skipped_tests[$name] = 'Skipped message: ' . $e->getMessage();
 
 	}
 
-    public function endTest(PHPUnit_Framework_Test $test, $time) {
+    public function endTest( PHPUnit_Framework_Test $test, $time ) {
 
        	$name = preg_replace( '(^(.*)::(.*?)$)', '\\1', $test->getName() );
        	$full_name = strpos( $test->getName(), '::' ) ? $test->getName() : $this->current_test_suite . '::' . $test->getName();
 
-		if( isset( $this->failed_tests[$full_name] ) ) {
+		if ( isset( $this->failed_tests[$full_name] ) ) {
        		$this->print_failed( $name . ' ' . $this->failed_tests[$full_name] );
 
-       	}
-       	elseif( isset( $this->skipped_tests[$full_name] ) ) {
+       	} elseif( isset( $this->skipped_tests[$full_name] ) ) {
        		$this->print_skipped( $name . ' ' . $this->skipped_tests[$full_name] );
-       	}
-       	else {
+
+       	} else {
        		$this->print_passed( $name );
        		$this->passed_tests[$name] = $name;
+
        	}
+
     }
 
     private function print_passed( $message ) {
 
-    	\cli\line( '%GPassed: %n' . $message );
+    	WP_CLI::line( '%GPassed: %n' . $message );
 
     }
 
     private function print_failed( $message ) {
 
-    	\cli\line( '%RFailed: %n' . $message );
+    	WP_CLI::line( '%RFailed: %n' . $message );
 
     }
 
     private function print_skipped( $message ) {
 
-    	\cli\line( '%YSkipped: %n' . $message );
+	    WP_CLI::line( '%YSkipped: %n' . $message );
 
     }
 
